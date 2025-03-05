9fda65db9ab7a18965b10ca90d6b714b93e79779
Akshay <akshay@olvy.co>
2025-03-05 21:11:24 +0530

    chore - test lint staged

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,22 @@
-import { createFileRoute } from "@tanstack/react-router";
-import RJSLogo from '/logo.svg'
+import { createFileRoute } from '@tanstack/react-router';
+import RJSLogo from '/logo.svg';
 
-export const Route = createFileRoute("/")({
-  component: Index,
+export const Route = createFileRoute('/')({
+	component: Index,
 });
 
 function Index() {
-  return (
-    <div className="pt-36 pb-12 flex flex-col gap-6 items-center justify-center text-white">
-      <div>
-        <img src={RJSLogo} className="logo" alt="RJS logo" />
-      </div>
+	return (
+		<div className='pt-36 pb-12 flex flex-col gap-6 items-center justify-center text-white'>
+			<div>
+				<img src={RJSLogo} className='logo' alt='RJS logo' />
+			</div>
 
-      <h1 className="text-3xl font-bold">React Jump Start</h1>
+			<h1 className='text-3xl font-bold'>React Jump Start</h1>
 
-      <div className="card">
-        list of included things - tailwind v4 - react 19 - react-router v7
-      </div>
-    </div>
-  );
+			<div className='card'>
+				list of included things - tailwind v4 - react 19 - react-router v7
+			</div>
+		</div>
+	);
 }
