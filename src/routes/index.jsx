e11fa84e3319010582373550aabd3bd66bc5f96b
Akshay <akshay@olvy.co>
2025-03-05 21:23:10 +0530

    add - stack info

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import RJSLogo from '/logo.svg';
+import { Link } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
 	component: Index,
@@ -14,8 +15,39 @@ function Index() {
 
 			<h1 className='text-3xl font-bold'>React Jump Start</h1>
 
-			<div className='card'>
-				list of included things - tailwind v4 - react 19 - react-router v7
+			<div className='card text-slate-500 font-medium text-lg flex items-center flex-wrap gap-2'>
+				<Link
+					to='https://react.dev/learn/installation'
+					target='_blank'
+					className='hover:text-blue-300 hover:underline'
+				>
+					React 19
+				</Link>
+
+				<span className=''>•</span>
+				<Link
+					to='https://tanstack.com/router/latest/docs/framework/react/installation'
+					target='_blank'
+					className='hover:text-blue-300 hover:underline'
+				>
+					Tanstack Router
+				</Link>
+				<span className=''>•</span>
+				<Link
+					to='https://tailwindcss.com/docs/installation/using-vite'
+					target='_blank'
+					className='hover:text-blue-300 hover:underline'
+				>
+					TailwindCSS v4
+				</Link>
+				<span className=''>•</span>
+				<Link
+					to='https://tailwindcss.com/docs/installation/using-vite'
+					target='_blank'
+					className='hover:text-blue-300 hover:underline'
+				>
+					Vite
+				</Link>
 			</div>
 		</div>
 	);
