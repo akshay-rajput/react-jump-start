cc1224c607e520139e28a862f2241c5f62e62b67
Akshay <akshay@olvy.co>
2025-03-05 19:35:55 +0530

    remove - unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 // Import the generated route tree
